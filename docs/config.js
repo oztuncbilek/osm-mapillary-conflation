2877278d1ab95cc67e02c9f7d78a213456feb839
oztuncbilek <tuncbilekozan@gmail.com>
2025-03-27 11:54:24 +0100

    fix: Rotate Mapbox access token

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 //Add Add public Mapbox token
 export const config = {
-   MAPBOX_TOKEN: 'pk.eyJ1Ijoib3pjYXJ0byIsImEiOiJjbThua2NodnowMWJ6MmtzZGNiMGY2amh4In0.IqKMaRT6tl6MKtv7uUHBnA'
+   MAPBOX_TOKEN: 'pk.eyJ1Ijoib3pjYXJ0byIsImEiOiJjbThyN2EzeXYwZ3o2MmpzaHlwdTh1ZGs3In0.8XnXk9UIQkx0c7SRxQcoAg'
 };
